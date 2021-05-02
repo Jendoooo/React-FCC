5ab92dd14d437ebb14a0207dff78679463dc48d5
Olajide Ayeola <olajideayeola@gmail.com>
2021-05-02 15:19:26 +0100

    introduction to mapping of components using the concept of higher order array

--- propspractice/src/App.js
+++ propspractice/src/App.js
@@ -1,31 +1,20 @@
 import React from "react"
 import Joke from "./components/joke"
-
+import jokesData from "./components/jokesdata"
 
 function App(){
+  // using the concept of higher order arrays to generate a new set of data
+  // now after the using this concept we want to save it to a variable so 
+  // we can use the new set of data generated 
+
+  // the jokeindexes inside of thie map bracktet below is the individual indexes data 
+  const jokeComponents =jokesData.map(jokeindexes=>{
+    return (<Joke question={jokeindexes.question} punchline={jokeindexes.punchline}/>)})
   
   return(
     <div>
-      
-      <Joke
-      qanda ={{question:"how MUCH is the joke",
-      punchline:"A suitable response "}}/>
-
-      <Joke
-      qanda ={{question:"another level",
-      punchline:"Olamide "}}/>
-
-      <Joke
-      qanda ={{
-      punchline:"Olamide wey no get question "}}/>
-      
-      <Joke
-      qanda ={{question:"Suddenly",
-      punchline:"Dbanj"}}/>
-      
-      <Joke
-      qanda ={{punchline:"Olamide "}}/>
-      
+      {/* react allows us to put the array of components in the jsx, so I will be donig that now  */}
+      {jokeComponents}
     </div>
   )
 }
---
+++ propspractice/src/components/jokesdata.js
@@ -0,0 +1,40 @@
+const jokesData = [
+    {
+        id:1,
+        question: "do you know why Bubu is called a Dullard ? ",
+        punchline:"Because he  is a strong one "
+    },
+    
+    {
+        id:2,
+        question: "do you know why Tinubu is called a Dullard ? ",
+        punchline:"Because he  is a strong one "
+    },
+    {
+        id:3,
+        question: "do you know why Saraki is called a Dullard ? ",
+        punchline:"Because he  is a strong one "
+    },
+    {
+        id:3,
+        question: "do you know why Saraki is called a Dullard ? ",
+        punchline:"Because he  is a strong one "
+    },
+    {
+        id:4,
+        punchline:"Because he  is a strong one "
+    },
+    {
+        id:5,
+        question: "do you know why APC is called a Dullard ? ",
+        punchline:"Because he  is a strong one "
+    },
+    {
+        id:6,
+        question: "PDP is called a Dullard ? ",
+        punchline:"Because he  is a strong one "
+    },
+
+]
+
+export default jokesData
